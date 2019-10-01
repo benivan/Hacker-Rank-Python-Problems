9c346fd37bd8c693d9a69d375aadcbc3c1f17577
Vikash Kumar <10918187+benivan@users.noreply.github.com>
2019-10-01 22:35:14 +0530

    Create itertools.combinations(iterable, r).py

---
+++ itertools.combinations(iterable, r).py
@@ -0,0 +1,50 @@
+'''
+This tool returns the  length subsequences of elements from the input iterable.
+
+Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+
+Task
+
+You are given a string .
+Your task is to print all possible combinations, up to size , of the string in lexicographic sorted order.
+
+Input Format
+
+A single line containing the string  and integer value  separated by a space.
+
+Constraints
+
+
+The string contains only UPPERCASE characters.
+
+Output Format
+
+Print the different combinations of string  on separate lines.
+
+Sample Input
+
+HACK 2
+'''
+>>> from itertools import combinations
+>>> 
+>>> print list(combinations('12345',2))
+[('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]
+>>> 
+>>> A = [1,1,3,3,3]
+>>> print list(combinations(A,4))
+[(1, 1, 3, 3), (1, 1, 3, 3), (1, 1, 3, 3), (1, 3, 3, 3), (1, 3, 3, 3)]
+
+
+
+from itertools import combinations
+abc=[str(x)for x in input().split()]
+two_words=list(combinations(sorted(abc[0].upper()),int(abc[1])))
+str_input=abc[0]
+for i in sorted(str_input):
+    print(i.upper())
+for j in two_words:
+        print("".join(j))
+
+
+
