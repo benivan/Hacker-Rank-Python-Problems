b77a706f71c6e2ab404c2a4700ed4db973e84d02
Vikash Kumar <10918187+benivan@users.noreply.github.com>
2019-10-17 04:02:47 +0530

    Update and rename Interquartile Range to Interquartile Range.py

--- Interquartile Range.py
+++ Interquartile Range.py
@@ -2,7 +2,7 @@ from statistics import median
 n=int(input())
 X=[int(x )for x in input().split()]
 Y=[int(y) for y in input().split()]
-XY=[]
+XY=sorted([])
 for i in range(n):
     XY+=[X[i]]*Y[i]
 if n%2 !=0:
